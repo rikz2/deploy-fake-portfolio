3c00a046c8d6d4fab9330d6327ab8c7fb21d9762
rik <sergio.certificaciones.estudio@gmail.com>
2024-05-28 01:57:21 -0500

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import PerfilInfo from './components/perfil-info'
+
+function App() {
+
+  return (
+    <>
+      <PerfilInfo></PerfilInfo>
+    </>
+  )
+}
+
+export default App
